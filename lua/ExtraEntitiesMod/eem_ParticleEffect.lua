e4f97ddfecd015a83bd28dceb2cf03c23c9f4131
Marco Pattke <j1m_w3st@web.de>
2013-03-08 18:08:57 +0100

    added LogicSwitch and emitterDestroyer

--- lua/ExtraEntitiesMod/eem_ParticleEffect.lua
+++ lua/ExtraEntitiesMod/eem_ParticleEffect.lua
@@ -5,11 +5,20 @@
 //
 //________________________________
 
+
+local networkVars =
+{
+    repeatStyle = "integer (0 to 2)",
+}
+
 if Server then
 
     local function EmitParticleEffect(self)
         // dont kill it if its endless or loop
         local effect = Shared.CreateEffect(nil, self.cinematicName, nil, self:GetCoords())
+        effect.repeatStyle = self.repeatStyle
+        effect.startsOnMessage = self.startsOnMessage
+        effect.listenChannel = self:GetListenChannel()
         if self.repeatStyle == 1 or self.repeatStyle == 2 then
             effect.lifeTime = -1
         end
@@ -29,3 +38,16 @@ if Server then
     
 end
 
+if Client then
+
+    local overrideParticleEffecOnInitialized = ParticleEffect.OnInitialized
+    function ParticleEffect:OnInitialized()
+        overrideParticleEffecOnInitialized(self)
+        if self.cinematic then
+            self.cinematic:SetRepeatStyle(self.repeatStyle or Cinematic.Repeat_Endless)
+        end
+    end
+
+end
+
+Class_Reload("ParticleEffect", networkVars)
