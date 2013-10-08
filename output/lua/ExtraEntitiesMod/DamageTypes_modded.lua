df187ed5344c60a9afb7cee89f97fe4a03490709
Alex Hayton <alex.hayton@gmail.com>
2013-10-08 20:54:03 +0100

    Made NPCs a tad easier. Added back hitPoint into the damage types.

--- output/lua/ExtraEntitiesMod/DamageTypes_modded.lua
+++ output/lua/ExtraEntitiesMod/DamageTypes_modded.lua
@@ -10,7 +10,7 @@ kNpcDamageDifficultyIncrease = 0.035
 
 // if its easy you only take half damage
 local originalGetDamageByType = GetDamageByType
-function GetDamageByType(target, attacker, doer, damage, damageType)
+function GetDamageByType(target, attacker, doer, damage, damageType, hitPoint)
     
 	// Store away whether the attacker was an NPC. This fixes bugs with e.g. lerk poison bite, grenades etc after the owner has died.
     if (attacker and attacker.isaNpc) or (doer and doer.damageFromNpc) then
@@ -24,5 +24,6 @@ function GetDamageByType(target, attacker, doer, damage, damageType)
 			damage = damage * 0.1
 		end
     end
-    return originalGetDamageByType(target, attacker, doer, damage, damageType)
+    return originalGetDamageByType(target, attacker, doer, damage, damageType, hitPoint)
+	
 end
